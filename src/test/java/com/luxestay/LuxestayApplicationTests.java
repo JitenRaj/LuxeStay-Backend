817d9e1d82892281caf9e0a03620b2a8620da515
JitenRaj <ji10raj@gmail.com>
2025-12-16 18:11:04 +0530

    add app tests

---
+++ src/test/java/com/luxestay/LuxestayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxestayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
