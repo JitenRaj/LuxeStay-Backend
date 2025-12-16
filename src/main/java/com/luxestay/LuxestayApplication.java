0a12961a062b0d90713ca41f1ce62b5d949fd5c0
JitenRaj <ji10raj@gmail.com>
2025-12-16 18:09:02 +0530

    add application

---
+++ src/main/java/com/luxestay/LuxestayApplication.java
@@ -0,0 +1,13 @@
+package com.luxestay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxestayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxestayApplication.class, args);
+	}
+
+}
